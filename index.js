6058a428678e5f2ae3f07bcefdf2fc9052da123d
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2024-02-13 21:30:10 -0800

    fix CORS

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const axios = require('axios');
-
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
 const apiKey = "905325ba3ffbdd89fba8248049958bff";
+
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get('/movie/:movieId', async (req, res) => {
   const movieId = req.params.movieId;
